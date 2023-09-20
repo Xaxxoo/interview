06f60382b74bf6288b3e6a834dc3658608661b8e
“Xaxxoo” <bnahmad83@gmail.com>
2023-09-20 23:45:15 +0100

    my interview submission

---
+++ src/components/feed/Feed.jsx
@@ -0,0 +1,31 @@
+import Post from "../post/Post";
+import Share from "../share/Share";
+import { useState, useEffect } from "react";
+
+import "./feed.css";
+
+export default function Feed() {
+  const [users, setData] = useState([]);
+
+  useEffect(() => {
+    const fetchUsers = async () => {
+      fetch("https://randomuser.me/api/")
+        .then((response) => response.json())
+        .then((data) => console.log(data));
+    };
+
+    setInterval(() => {
+      fetchUsers();
+    }, 5000);
+  }, []);
+  return (
+    <div className="feed">
+      <div className="feedWrapper">
+        <Share />
+        {users.map((user) => (
+          <Post key={user.id} post={user} />
+        ))}
+      </div>
+    </div>
+  );
+}
